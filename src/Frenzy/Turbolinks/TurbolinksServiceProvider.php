53a4d904a9a13103fb857df67e2344586f17e3ba
Mathieu Doyon <mathieu.doyon@studiofrenetic.com>
2014-01-29 10:42:17 -0500

    Convert indetation to 4 spaces
    Signed-off-by: Mathieu Doyon <mathieu.doyon@studiofrenetic.com>

--- src/Frenzy/Turbolinks/TurbolinksServiceProvider.php
+++ src/Frenzy/Turbolinks/TurbolinksServiceProvider.php
@@ -4,35 +4,35 @@ use Illuminate\Support\ServiceProvider;
 
 class TurbolinksServiceProvider extends ServiceProvider {
 
-	/**
-	 * Indicates if loading of the provider is deferred.
-	 *
-	 * @var bool
-	 */
-	protected $defer = false;
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = false;
 
-	/**
-	 * Bootstrap the application events.
-	 *
-	 * @return void
-	 */
-	public function boot()
-	{
-		$this->package('frenzy/turbolinks');
+    /**
+     * Bootstrap the application events.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->package('frenzy/turbolinks');
 
         /** @var Turbolinks $turbolinks */
         $turbolinks = $this->app['turbolinks'];
         $turbolinks->boot();
-	}
+    }
 
-	/**
-	 * Register the service provider.
-	 *
-	 * @return void
-	 */
-	public function register()
-	{
-		$app = $this->app;
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $app = $this->app;
         $this->app['turbolinks'] = $this->app->share(function ($app){
             $turbolinks = new Turbolinks($app);
 
@@ -43,16 +43,16 @@ class TurbolinksServiceProvider extends ServiceProvider {
             $app->middleware('Frenzy\Turbolinks\Middleware', array($app));
         }
 
-	}
+    }
 
-	/**
-	 * Get the services provided by the provider.
-	 *
-	 * @return array
-	 */
-	public function provides()
-	{
-		return array('turbolinks');
-	}
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return array('turbolinks');
+    }
 
 }
